fd79ebe2d54de2f604a8c508e6d04d965a9d7208
neo <iamhahar@gmail.com>
2018-08-23 23:36:03 +0800

    test

--- testlib/src/testapi.cpp
+++ testlib/src/testapi.cpp
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <SFML/Graphics.hpp>
 
 void test()
 {
+  std::unique_ptr<sf::RenderWindow> main_window;
+  sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+
+  main_window = std::make_unique<sf::RenderWindow>(sf::VideoMode(desktop.width, desktop.height, desktop.bitsPerPixel)
+            , "Title", sf::Style::Fullscreen);
   printf("test\n");
 }
